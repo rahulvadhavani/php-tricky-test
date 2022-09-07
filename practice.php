0acfcfeceb1a7209166e530fc1ddf60b515ed673
rahulvadhavani <rb.vadhavani@gmail.com>
2022-09-07 09:51:47 +0530

    add fibonacci series

--- practice.php
+++ practice.php
@@ -1,34 +1,37 @@
 <?php
 
-/*each year has 13 months
-each even month has 21 days, and each odd month has 22
-the 13th month lacks a day every leap year
-a leap year is any year divisible by 5
-each week has 7 days: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
-The task went as follows:
-
-Given that the first day of the year 1900 was tuesday, write a function that will print the day
-of the week for a given date. Example, for input {day: 17, month: 11, year: 2013} the output is 
-"Saturday".
-*/
-
-$year = "13 months";
-$range = range(1, 13);
-// print_r($range);
-// 1,3,5,7,9,11,13 = 21
-// 2,4,6,8,10,12 = 22
-
-$mydate = '1-jan-1900';
-$day = getDay($mydate);
-// echo date('l', strtotime('first day of january 1900')); echo '<br>';
-function getDay($mydate)
-{
-    $mydate2 = '8-jun-1900';
-
-    // print_r(date_create("2013-03-15"));
-    $newdate = date('Y-m-d', strtotime($mydate));
-    $year = date('Y', strtotime($mydate));
-    $month = date('m', strtotime($mydate));
-    $date = date('d', strtotime($mydate));
-    return $year;
+// bubble sorting algorithem
+function sortMe($arr = []){
+    $n = count($arr);
+    for ($i = 0; $i < $n-1; $i++) {
+        for ($j = 0; $j < $n-1-$i; $j++) {
+            if($arr[$j] > $arr[$j+1]){
+                $tmp = $arr[$j];
+                $arr[$j] = $arr[$j+1];
+                $arr[$j+1] = $tmp;
+            }
+        }
+    }
+    return $arr;
 }
+
+// $arr = sortMe([5,1,25,6,33,100,9,7,2,3]);
+// print_r($arr);
+
+
+// fibonacci series
+function fibonacci(int $elements){
+    $arr = [0,1];
+    for ($i = 0; $i < $elements-2; $i++) {
+        $arr[] = $arr[$i] + $arr[$i+1];
+    }
+    $arr =  implode(',',$arr);
+    return $arr;
+}
+
+
+// 0, 1, 1,2,3,5,8,13,21
+
+$fibonacciser = fibonacci(12);
+print_r($fibonacciser);
+
