48e4406b57e248de77bf2378e47fad9a3ecbf89a
rahulvadhavani <rb.vadhavani@gmail.com>
2022-09-07 13:14:33 +0530

    add number to word method

--- practice.php
+++ practice.php
@@ -1,14 +1,15 @@
 <?php
 
 // bubble sorting algorithem
-function sortMe($arr = []){
+function sortMe($arr = [])
+{
     $n = count($arr);
-    for ($i = 0; $i < $n-1; $i++) {
-        for ($j = 0; $j < $n-1-$i; $j++) {
-            if($arr[$j] > $arr[$j+1]){
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - 1 - $i; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
                 $tmp = $arr[$j];
-                $arr[$j] = $arr[$j+1];
-                $arr[$j+1] = $tmp;
+                $arr[$j] = $arr[$j + 1];
+                $arr[$j + 1] = $tmp;
             }
         }
     }
@@ -20,18 +21,78 @@ function sortMe($arr = []){
 
 
 // fibonacci series
-function fibonacci(int $elements){
-    $arr = [0,1];
-    for ($i = 0; $i < $elements-2; $i++) {
-        $arr[] = $arr[$i] + $arr[$i+1];
+function fibonacci(int $elements)
+{
+    $arr = [0, 1];
+    for ($i = 0; $i < $elements - 2; $i++) {
+        $arr[] = $arr[$i] + $arr[$i + 1];
     }
-    $arr =  implode(',',$arr);
+    $arr =  implode(',', $arr);
     return $arr;
 }
 
+//ex.:  0, 1, 1,2,3,5,8,13,21
 
-// 0, 1, 1,2,3,5,8,13,21
+// $fibonacciser = fibonacci(12);
+// print_r($fibonacciser);
 
-$fibonacciser = fibonacci(12);
-print_r($fibonacciser);
 
+
+/*
+*number to word
+*source : https://reeteshghimire.com.np/2021/01/25/convert-number-into-words-using-php
+
+*/
+function convertNumber($num = false)
+{
+    $num = str_replace(array(',', ''), '', trim($num));
+    if (!$num) {
+        return false;
+    }
+    $num = (int) $num;
+    $words = array();
+    $list1 = array(
+        '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+        'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    );
+    $list2 = array('', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred');
+    $list3 = array(
+        '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion',
+        'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quattuordecillion',
+        'quindecillion', 'sexdecillion', 'septendecillion', 'octodecillion', 'novemdecillion', 'vigintillion'
+    );
+    $num_length = strlen($num);
+    $levels = (int) (($num_length + 2) / 3);
+    $max_length = $levels * 3;
+    $num = substr('00' . $num, -$max_length);
+    $num_levels = str_split($num, 3);
+    for ($i = 0; $i < count($num_levels); $i++) {
+        $levels--;
+        $hundreds = (int) ($num_levels[$i] / 100);
+        $hundreds = ($hundreds ? ' ' . $list1[$hundreds] . ' hundred' . ($hundreds == 1 ? '' : '') . ' ' : '');
+        $tens = (int) ($num_levels[$i] % 100);
+        $singles = '';
+        if ($tens < 20) {
+            $tens = ($tens ? ' and ' . $list1[$tens] . ' ' : '');
+        } elseif ($tens >= 20) {
+            $tens = (int)($tens / 10);
+            $tens = ' and ' . $list2[$tens] . ' ';
+            $singles = (int) ($num_levels[$i] % 10);
+            $singles = ' ' . $list1[$singles] . ' ';
+        }
+        $words[] = $hundreds . $tens . $singles . (($levels && (int) ($num_levels[$i])) ? ' ' . $list3[$levels] . ' ' : '');
+    } //end for loop
+    $commas = count($words);
+    if ($commas > 1) {
+        $commas = $commas - 1;
+    }
+    $words = implode(' ',  $words);
+    $words = preg_replace('/^\s\b(and)/', '', $words);
+    $words = trim($words);
+    $words = ucfirst($words);
+    $words = $words . ".";
+    return $words;
+}
+
+// $res = convertNumber(123456);
+// echo $res;
